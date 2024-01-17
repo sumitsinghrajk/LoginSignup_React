b6838e815f25ae7e97d25583b6ffbf86b30f41b7
Sumit Singh <sumitsinghr3007@gmail.com>
2024-01-17 12:04:04 +0530

    first commit reactsite

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+
+function App() {
+  return (
+    <div >
+      <LoginSignup/>
+      <index/>
+    </div>
+  );
+}
+
+export default App;
